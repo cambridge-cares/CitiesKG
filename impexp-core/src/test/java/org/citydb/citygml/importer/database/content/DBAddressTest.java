ec66d01f152462bef4c63dd778fb39275dccf038
Arkadiusz Chadzynski <arkadiusz.chadzynski@cares.cam.ac.uk>
2020-12-03 10:54:42 +0800

    Added DBAddressTest improvements and code clean up

--- impexp-core/src/test/java/org/citydb/citygml/importer/database/content/DBAddressTest.java
+++ impexp-core/src/test/java/org/citydb/citygml/importer/database/content/DBAddressTest.java
@@ -1,7 +1,6 @@
 package org.citydb.citygml.importer.database.content;
 
 import org.citydb.config.Config;
-import org.citydb.database.version.DatabaseVersion;
 import org.junit.jupiter.api.Test;
 
 import java.lang.reflect.Method;
@@ -9,9 +8,10 @@ import java.sql.Connection;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class DBAddressTest {
+public class DBAddressTest {
+
     @Test
-    void getSPARQLStatementTest() throws Exception {
+    public void getSPARQLStatementTest() {
 
         String expected = "PREFIX ocgml: <http://locahost/ontocitygml/> " +
                 "BASE <http://localhost/berlin/> " +
@@ -19,29 +19,34 @@ class DBAddressTest {
                 "GRAPH <address/> { ? ocgml:id  ?;ocgml:gmlId  ?;ocgml:street  ?;ocgml:houseNumber  ?;" +
                 "ocgml:poBox  ?;ocgml:zipCode  ?;ocgml:city  ?;ocgml:country  ?;ocgml:multiPoint  ?;" +
                 "ocgml:xalSource  ?;.}}";
-        String generated = "";
+        String generated;
 
-        // Set up the environment
-        CityGMLImportManager importer = DBObjectTestHelper.getCityGMLImportManager();
-        Config config = DBObjectTestHelper.getConfig();
-        Connection batchConn = DBObjectTestHelper.getConnection();
+        try {
+            // Set up the environment
+            CityGMLImportManager importer = DBObjectTestHelper.getCityGMLImportManager();
+            Config config = DBObjectTestHelper.getConfig();
+            Connection batchConn = DBObjectTestHelper.getConnection();
 
-        DBAddress dbAddress = new DBAddress(batchConn, config, importer);
+            DBAddress dbAddress = new DBAddress(batchConn, config, importer);
 
-        assertNotNull(dbAddress.getClass().getDeclaredMethod("getSPARQLStatement", String.class));
-        Method getsparqlMethod = DBAddress.class.getDeclaredMethod("getSPARQLStatement", String.class);
-        getsparqlMethod.setAccessible(true);
+            assertNotNull(dbAddress.getClass().getDeclaredMethod("getSPARQLStatement", String.class));
+            Method getsparqlMethod = DBAddress.class.getDeclaredMethod("getSPARQLStatement", String.class);
+            getsparqlMethod.setAccessible(true);
 
-        // Prepare gmlIdCodespace for getSPARQLStatement method
-        boolean hasGmlIdColumn = importer.getDatabaseAdapter().getConnectionMetaData().getCityDBVersion().compareTo(3, 1, 0) >= 0;
-        String gmlIdCodespace = null;
-        if (hasGmlIdColumn) {
-            gmlIdCodespace = config.getInternal().getCurrentGmlIdCodespace();
-            if (gmlIdCodespace != null)
-                gmlIdCodespace = "'" + gmlIdCodespace + "', ";
+            // Prepare gmlIdCodespace for getSPARQLStatement method
+            boolean hasGmlIdColumn = importer.getDatabaseAdapter().getConnectionMetaData().getCityDBVersion().compareTo(3, 1, 0) >= 0;
+            String gmlIdCodespace = null;
+            if (hasGmlIdColumn) {
+                gmlIdCodespace = config.getInternal().getCurrentGmlIdCodespace();
+                if (gmlIdCodespace != null)
+                    gmlIdCodespace = "'" + gmlIdCodespace + "', ";
+            }
+
+            generated = (String) getsparqlMethod.invoke(dbAddress, gmlIdCodespace);
+            assertEquals(expected, generated);
+        } catch (Exception e) {
+            fail();
         }
-
-        generated = (String) getsparqlMethod.invoke(dbAddress, gmlIdCodespace);
-        assertEquals(expected, generated);
     }
+
 }
