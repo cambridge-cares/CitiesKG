dc3333ebace04153eb757a5f0e5aa4b598ae544d
Arkadiusz Chadzynski <arkadiusz.chadzynski@cares.cam.ac.uk>
2021-01-19 18:29:27 +0800

    Added loading existing datatypes and vocabulary entries on start to the config builder.

--- impexp-core/src/main/java/org/citydb/database/adapter/blazegraph/BlazegraphConfigBuilder.java
+++ impexp-core/src/main/java/org/citydb/database/adapter/blazegraph/BlazegraphConfigBuilder.java
@@ -1,37 +1,45 @@
 package org.citydb.database.adapter.blazegraph;
 
+import org.apache.log4j.Logger;
 import org.json.JSONArray;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
 
+/**
+ * Config builder for Blazegraph with double-check locking private instance so that it can be
+ * accessed by only by getInstance() method. This ensures thread safe singleton.
+ *
+ * @author <a href="mailto:arkadiusz.chadzynski@cares.cam.ac.uk">Arkadiusz Chadzynski</a>
+ * @version $Id$
+ */
 public class BlazegraphConfigBuilder {
-
-    // Config builder for Blazegraph with double-check locking private instance so that it can be
-    // accessed by only by getInstance() method. This ensures thread safe singleton.
+    private static final transient Logger log = Logger.getLogger(BlazegraphConfigBuilder.class);
+    protected final String CFG_ERR = "Could not load ";
     private static BlazegraphConfigBuilder instance;
     private final Set<String> geoDataTypes;
     private final Set<String> uriStrings;
 
-    private BlazegraphConfigBuilder()
-    {
+    private BlazegraphConfigBuilder() {
         // private constructor
         geoDataTypes = new HashSet<>();
         uriStrings = new HashSet<>();
     }
 
-    public static BlazegraphConfigBuilder getInstance()
-    {
-        if (instance == null)
-        {
+    public static BlazegraphConfigBuilder getInstance() {
+        if (instance == null) {
             //synchronized block to remove overhead
-            synchronized (BlazegraphConfigBuilder.class)
-            {
-                if(instance==null)
-                {
+            synchronized (BlazegraphConfigBuilder.class) {
+                if(instance==null) {
                     // if instance is null, initialize
                     instance = new BlazegraphConfigBuilder();
+                    //load existing configuration:
+                    instance.loadURIs();
+                    instance.loadDatatypes();
                 }
             }
         }
@@ -63,6 +71,52 @@ public class BlazegraphConfigBuilder {
     public Properties build() {
         BlazegraphConfig config = new BlazegraphConfig(geoDataTypes);
         return config.getConfig();
+    }
+
+    private void loadURIs() {
+        String path;
+        String key;
+        try {
+            path = BlazegraphAdapter.BLAZEGRAPH_VOCAB_CFG_PATH;
+            key = BlazegraphAdapter.BLAZEGRAPH_VOCAB_CFG_KEY_URIS;
+            Properties prop = loadProperties(path);
+
+            JSONArray uris = new JSONArray(prop.getProperty(key));
+
+            //add vocabulary declarations to existing uriStrings set
+            for (Object uri: uris) {
+                uriStrings.add(uri.toString());
+            }
+        } catch (IOException e) {
+            log.error(CFG_ERR + BlazegraphAdapter.BLAZEGRAPH_VOCAB_CFG_PATH);
+        }
+    }
+
+    private void loadDatatypes() {
+        String path;
+        try {
+            path = BlazegraphAdapter.BLAZEGRAPH_CFG_PATH;
+            Properties prop = loadProperties(path);
+            //extract datatype configs and to existing geoDataTypes set
+            for (Object cfg: loadProperties(path).stringPropertyNames().toArray()) {
+                if (String.valueOf(cfg).matches(BlazegraphGeoDatatype.KEY_MAIN+"[0-9]{1}")) {
+                    geoDataTypes.add(prop.getProperty(String.valueOf(cfg)));
+                }
+            }
+
+        } catch (IOException e) {
+            log.error(CFG_ERR + BlazegraphAdapter.BLAZEGRAPH_CFG_PATH);
+        }
+    }
+
+
+    private Properties loadProperties(String path) throws IOException {
+        //try loading properties file
+        InputStream input = new FileInputStream(path);
+        Properties prop = new Properties();
+        prop.load(input);
+
+        return prop;
     }
 
 }
