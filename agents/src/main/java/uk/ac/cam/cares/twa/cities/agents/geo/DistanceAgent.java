df624b9198b72470fe42d2e58fd26626f773cfc4
Ayda Grisiute <ayda.grisiute@sec.ethz.ch>
2021-05-10 12:56:26 +0800

    Add DistanceAgent class description. Move distance agent into correct package folder.

---
+++ agents/src/main/java/uk/ac/cam/cares/twa/cities/agents/geo/DistanceAgent.java
@@ -0,0 +1,10 @@
+package uk.ac.cam.cares.twa.cities.agents.geo;
+
+
+/** This comment describes the DistanceAgent class.
+ *  Two unique IRIs are used as inputs for the DistanceAgent class.
+ *  First, the agent checks if the distance already exists between two IRIs and if not, it uses the envelope string of each IRI to create two envelope objects and calculate the distance between them.
+ *  Second, the DistanceAgent class inserts the resulted distance into the Blazegraph and also returns it.
+ */
+public class DistanceAgent {
+}
