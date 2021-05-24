f91667bcee9640b22f4e3be72916579c13f4979f
Arkadiusz Chadzynski <arkadiusz.chadzynski@cares.cam.ac.uk>
2021-05-24 11:37:21 +0800

    Made DBSurfaceData DBSurfaceGeometry SPARQL URLs built from connection parameters.

--- impexp-core/src/main/java/org/citydb/citygml/importer/database/content/DBSurfaceGeometry.java
+++ impexp-core/src/main/java/org/citydb/citygml/importer/database/content/DBSurfaceGeometry.java
@@ -112,11 +112,10 @@ public class DBSurfaceGeometry implements DBImporter {
 	private String nullGeometryTypeName;
 	private LocalAppearanceHandler localAppearanceHandler;
 	private RingValidator ringValidator;
-	//@todo Replace graph IRI and OOntocityGML prefix with variables set on the GUI
-	private static final String IRI_GRAPH_BASE = "http://localhost/berlin/";
-	private static final String PREFIX_ONTOCITYGML = "http://locahost/ontocitygml/";
+	private String PREFIX_ONTOCITYGML;
+	private String IRI_GRAPH_BASE;
+	private String IRI_GRAPH_OBJECT;
 	private static final String IRI_GRAPH_OBJECT_REL = "surfacegeometry/";
-	public static final String IRI_GRAPH_OBJECT = IRI_GRAPH_BASE + IRI_GRAPH_OBJECT_REL;
 
 	public DBSurfaceGeometry(Connection batchConn, Config config, CityGMLImportManager importer) throws CityGMLImportException, SQLException {
 		this.batchConn = batchConn;
@@ -151,6 +150,9 @@ public class DBSurfaceGeometry implements DBImporter {
 		stmt.append("?, ?)");
 
 		if (importer.isBlazegraph()) {
+			PREFIX_ONTOCITYGML = importer.getOntoCityGmlPrefix();
+			IRI_GRAPH_BASE = importer.getGraphBaseIri();
+			IRI_GRAPH_OBJECT = IRI_GRAPH_BASE + IRI_GRAPH_OBJECT_REL;
 			stmt = getSPARQLStatement(stmt);
 		}
 
