40ec5cf82f22f0cb91652c2d495f4a713d670702
Arkadiusz Chadzynski <arkadiusz.chadzynski@cares.cam.ac.uk>
2021-07-27 18:07:48 +0800

    Started N-Quads upload implementation.

--- agents/src/main/java/uk/ac/cam/cares/twa/cities/tasks/NquadsUploaderTask.java
+++ agents/src/main/java/uk/ac/cam/cares/twa/cities/tasks/NquadsUploaderTask.java
@@ -1,6 +1,14 @@
 package uk.ac.cam.cares.twa.cities.tasks;
 
+import org.apache.commons.io.FileUtils;
+import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+
+import javax.ws.rs.HttpMethod;
 import java.io.File;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URI;
 import java.util.concurrent.BlockingQueue;
 
@@ -8,6 +16,9 @@ public class NquadsUploaderTask implements Runnable {
     private Boolean stop = false;
     private final BlockingQueue<File> nqQueue;
     private final URI endpointUri;
+    private final String HEADER_CTYPE = "Content-Type";
+    private final String HEADER_CLEN = "Content-Length";
+    private final String CTYPE_NQ = "text/x-nquads";
 
     public NquadsUploaderTask(BlockingQueue<File> nqQueue, URI endpointUri) {
         this.nqQueue = nqQueue;
@@ -21,8 +32,28 @@ public class NquadsUploaderTask implements Runnable {
     @Override
     public void run() {
         while (!stop) {
-            //@todo implementation
-            stop();
+            while (!nqQueue.isEmpty()) {
+                try {
+                    File nqFile = nqQueue.take();
+
+                    //@todo implementatio
+
+                    HttpURLConnection conn = (HttpURLConnection) endpointUri.toURL().openConnection();
+                    OutputStream os = conn.getOutputStream();
+                    conn.setDoOutput(true);
+                    conn.setRequestMethod(HttpMethod.POST);
+                    conn.setRequestProperty(HEADER_CTYPE, CTYPE_NQ);
+                    conn.setRequestProperty(HEADER_CLEN, String.valueOf(nqFile.length()));
+                    os.write(FileUtils.readFileToByteArray(nqFile));
+
+
+
+                } catch (InterruptedException | IOException e) {
+                    throw  new JPSRuntimeException(e);
+                } finally {
+                    stop();
+                }
+            }
         }
     }
 }
