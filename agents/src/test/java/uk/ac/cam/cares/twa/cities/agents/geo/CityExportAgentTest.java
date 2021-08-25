f0d2c6b57f5ab38e0f6b4a96566d7ac0484e28b6
shiyingli <shiying.li@sec.ethz.ch>
2021-08-25 13:47:10 +0800

    Initialize the unit tests

--- agents/src/test/java/uk/ac/cam/cares/twa/cities/agents/geo/CityExportAgentTest.java
+++ agents/src/test/java/uk/ac/cam/cares/twa/cities/agents/geo/CityExportAgentTest.java
@@ -1,4 +1,20 @@
+package uk.ac.cam.cares.twa.cities.agents.geo;
+
+import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
-class CityExportAgentTest {
-  
+
+class CityExportAgentTest extends TestCase {
+
+
+    @Test
+    public void testNewCityExporttAgent() {}
+
+    @Test
+    public void testValidateListenInput()  {}
+
+    @Test
+    public void testexportKml() {}
+
 }
--- agents/src/test/java/uk/ac/cam/cares/twa/cities/tasks/ExporterTaskTest.java
+++ agents/src/test/java/uk/ac/cam/cares/twa/cities/tasks/ExporterTaskTest.java
@@ -1,4 +1,9 @@
+package uk.ac.cam.cares.twa.cities.tasks;
+
+import junit.framework.TestCase;
+
 import static org.junit.jupiter.api.Assertions.*;
-class ExporterTaskTest {
-  
+
+class ExporterTaskTest extends TestCase {
+
 }
