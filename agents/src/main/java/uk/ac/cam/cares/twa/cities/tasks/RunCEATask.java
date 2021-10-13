38dcc29b1483ed9c26d7d28f5e568d5819ef6aac
lloydejw <ejwl2@cam.ac.uk>
2021-10-13 12:44:08 +0800

    Implement running CEA create_new_scenario script using CommandHelper

--- agents/src/main/java/uk/ac/cam/cares/twa/cities/tasks/RunCEATask.java
+++ agents/src/main/java/uk/ac/cam/cares/twa/cities/tasks/RunCEATask.java
@@ -1,6 +1,9 @@
 package uk.ac.cam.cares.twa.cities.tasks;
 
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import uk.ac.cam.cares.jps.base.util.CommandHelper;
+
+import java.io.File;
 import java.util.ArrayList;
 
 public class RunCEATask implements Runnable {
@@ -22,6 +25,16 @@ public class RunCEATask implements Runnable {
         while (!stop) {
 
             try {
+                String CMDER_ROOT = "C:\\Users\\ELLO01\\Documents\\CityEnergyAnalyst\\Dependencies\\cmder";
+                String FilePath = new File(CMDER_ROOT, "test_new.bat").getAbsolutePath();
+
+                // Create a new scenario
+                String createNewScenarioArgs = "cea create_new_scenario --project testProject1 --scenario testScenario --output-path  C:\\Users\\ELLO01\\Documents\\testProject";
+                ArrayList<String> args = new ArrayList<>();
+                args.add(FilePath);
+                args.add(createNewScenarioArgs);
+
+                CommandHelper.executeCommands("C:\\Users\\ELLO01\\Documents\\testProject\\testProject1\\testScenario\\inputs\\building-geometry", args);
 
             } catch ( NullPointerException e) {
                 e.printStackTrace();
