450e03e83145ce421701597afb6f61efc57f0f99
lloydejw <ejwl2@cam.ac.uk>
2021-10-22 09:29:39 +0800

    Run all CEA scripts in RunCEATask

--- agents/src/main/java/uk/ac/cam/cares/twa/cities/tasks/RunCEATask.java
+++ agents/src/main/java/uk/ac/cam/cares/twa/cities/tasks/RunCEATask.java
@@ -3,8 +3,10 @@ package uk.ac.cam.cares.twa.cities.tasks;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import com.google.gson.Gson;
 
+import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
@@ -53,11 +55,6 @@ public class RunCEATask implements Runnable {
         while (!stop) {
 
             try {
-
-                // Create a new scenario
-                String createNewScenarioArgs = "cea create_new_scenario --project testProject1 --scenario testScenario --output-path  C:\\Users\\ELLO01\\Documents\\testProject";
-                runCEAScript(createNewScenarioArgs);
-
                 // Create dummy input data
                 CEAInputData result = new CEAInputData();
 
@@ -70,7 +67,7 @@ public class RunCEATask implements Runnable {
                 String FilePath = new File("C:\\Users\\ELLO01\\Documents\\CitiesKG\\utils", "create_shapefile.py").getAbsolutePath();
 
                 ArrayList<String> args2 = new ArrayList<>();
-                args2.add("python3");
+                args2.add("python");
                 args2.add(FilePath);
                 args2.add(dataString.replace("\"", "\\\""));
 
@@ -79,7 +76,30 @@ public class RunCEATask implements Runnable {
                 // starting the process
                 build.start();
 
-            } catch ( IOException | NullPointerException e) {
+                // Create a new scenario and run CEA scripts
+                String createNewScenarioArgs = "cea create_new_scenario --project testProject1 --scenario testScenario --output-path  C:\\Users\\ELLO01\\Documents\\testProject --zone C:\\Users\\ELLO01\\Documents\\testProject\\zone.shp";
+                runCEAScript(createNewScenarioArgs);
+                String weatherHelperArgs = "cea weather-helper --scenario C:\\Users\\ELLO01\\Documents\\testProject\\testProject1\\testScenario";
+                runCEAScript(weatherHelperArgs);
+                String surroundingsHelperArgs = "cea surroundings-helper --scenario C:\\Users\\ELLO01\\Documents\\testProject\\testProject1\\testScenario --buffer 250 --height-ag --floors-ag ";
+                runCEAScript(surroundingsHelperArgs);
+                String terrainHelperArgs = "cea terrain-helper --scenario C:\\Users\\ELLO01\\Documents\\testProject\\testProject1\\testScenario";
+                runCEAScript(terrainHelperArgs);
+                String databaseInitializerArgs = "cea data-initializer --scenario C:\\Users\\ELLO01\\Documents\\testProject\\testProject1\\testScenario --databases-path C:\\Users\\ELLO01\\Documents\\cityenergyanalyst\\cityenergyanalyst\\cea\\databases\\SG";
+                runCEAScript(databaseInitializerArgs);
+                String archetypesMapperArgs = "cea archetypes-mapper --scenario C:\\Users\\ELLO01\\Documents\\testProject\\testProject1\\testScenario --buildings ['B001']";
+                runCEAScript(archetypesMapperArgs);
+                String radiationArgs = "cea radiation --scenario C:\\Users\\ELLO01\\Documents\\testProject\\testProject1\\testScenario --buildings ['B001']";
+                runCEAScript(radiationArgs);
+                String scheduleMakerArgs = "cea schedule-maker --scenario C:\\Users\\ELLO01\\Documents\\testProject\\testProject1\\testScenario --buildings ['B001']";
+                runCEAScript(scheduleMakerArgs);
+                String demandArgs = "cea demand --scenario C:\\Users\\ELLO01\\Documents\\testProject\\testProject1\\testScenario --buildings ['B001'] --loads-output ['GRID', 'QH_sys', 'QC_sys']";
+                runCEAScript(demandArgs);
+                String PVArgs = "cea photovoltaic-thermal --scenario C:\\Users\\ELLO01\\Documents\\testProject\\testProject1\\testScenario --buildings ['B001']";
+                runCEAScript(PVArgs);
+
+
+            } catch (  IOException | NullPointerException e) {
                 e.printStackTrace();
                 throw new JPSRuntimeException(e);
             } finally {
