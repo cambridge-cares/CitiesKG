439c89fe798830190e1438b5358d3039bf671b83
Ayda Grisiute <ayda.grisiute@sec.ethz.ch>
2021-11-19 15:51:28 +0800

    added code to processRequestParameters for creating city object and filling its scallars and collections. Added setKgClient() method.

--- agents/src/main/java/uk/ac/cam/cares/twa/cities/agents/geo/CityInformationAgent.java
+++ agents/src/main/java/uk/ac/cam/cares/twa/cities/agents/geo/CityInformationAgent.java
@@ -9,6 +9,9 @@ import javax.ws.rs.HttpMethod;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import uk.ac.cam.cares.jps.base.agent.JPSAgent;
+import uk.ac.cam.cares.jps.base.interfaces.KnowledgeBaseClientInterface;
+import uk.ac.cam.cares.jps.base.query.KGRouter;
+import uk.ac.cam.cares.twa.cities.model.geo.CityObject;
 
 
 /**
@@ -16,14 +19,16 @@ import uk.ac.cam.cares.jps.base.agent.JPSAgent;
  */
 
 
-@WebServlet(urlPatterns = {CityInformationAgent.URI_CITY_INFORMATION})
+@WebServlet(urlPatterns = {CityInformationAgent.URI_CITY_OBJECT_INFORMATION})
 public class CityInformationAgent extends JPSAgent {
 
-  public static final String URI_CITY_INFORMATION = "/cityinformation";
+  public static final String URI_CITY_OBJECT_INFORMATION = "/cityobjectinformation";
   public static final String KEY_REQ_METHOD = "method";
   public static final String KEY_IRIS = "iris";
   public static final String KEY_ATTRIBUTES = "attributes";
 
+  private KnowledgeBaseClientInterface kgClient;
+  private static String route = "http://kb/citieskg-berlin";
 
   @Override
   public JSONObject processRequestParameters(JSONObject requestParams) {
@@ -35,11 +40,21 @@ public class CityInformationAgent extends JPSAgent {
     for (Object iri : iris) {
       uris.add(iri.toString());
     }
-    JSONArray attributes = new JSONArray();
+    JSONArray cityInformation = new JSONArray(); // ask Arek
 
-    // agent specific code to be added.
+    setKGClient(true);
 
-    requestParams.append(KEY_ATTRIBUTES, attributes);
+    for (int iri = 0; iri < uris.size(); iri++) {
+      String cityObjectIri= uris.get(iri);
+      CityObject cityObject =  new CityObject(cityObjectIri);
+
+      cityObject.fillScalars(cityObjectIri, kgClient);
+      cityObject.fillCollections(cityObjectIri,kgClient, false);
+
+      //getters to extract info and put it to JSONArray.
+      //Ask Arek how to package every in JSON Object
+    }
+    requestParams.append(KEY_ATTRIBUTES, cityInformation);
     return requestParams;
   }
 
@@ -65,6 +80,14 @@ public class CityInformationAgent extends JPSAgent {
     throw new BadRequestException();
   }
 
-  // add method signatures
+  /**
+   * sets KG Client for specific endpoint.
+   *
+   * @param isQuery boolean
+   */
+  private void setKGClient(boolean isQuery) {
+
+    this.kgClient = KGRouter.getKnowledgeBaseClient(route, isQuery, !isQuery);
+  }
 
 }
