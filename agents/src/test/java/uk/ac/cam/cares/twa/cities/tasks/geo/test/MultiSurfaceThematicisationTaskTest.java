7c3deae51c253fcdce64a75c689d7f2691736661
Mehal Agarwal <99166247+mehalaga@users.noreply.github.com>
2022-04-26 12:15:05 +0800

    104-thematicsurfacediscoveryagent: Upgrade unit tests in MultiSurfaceThematicisationTaskTest to JUnit 5

--- agents/src/test/java/uk/ac/cam/cares/twa/cities/tasks/geo/test/MultiSurfaceThematicisationTaskTest.java
+++ agents/src/test/java/uk/ac/cam/cares/twa/cities/tasks/geo/test/MultiSurfaceThematicisationTaskTest.java
@@ -1,6 +1,6 @@
 package uk.ac.cam.cares.twa.cities.tasks.geo.test;
 
-import junit.framework.TestCase;
+import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
 import uk.ac.cam.cares.twa.cities.agents.geo.ThematicSurfaceDiscoveryAgent;
 import uk.ac.cam.cares.twa.cities.models.ModelContext;
@@ -15,9 +15,9 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.*;
 
-public class MultiSurfaceThematicisationTaskTest extends TestCase {
+public class MultiSurfaceThematicisationTaskTest{
 
   static Field contextField;
   static Field rootsField;
@@ -48,6 +48,7 @@ public class MultiSurfaceThematicisationTaskTest extends TestCase {
     return mockContext;
   }
 
+  @Test
   public void testTryClassifyGeometries() throws NoSuchFieldException, IllegalAccessException {
 
     ModelContext context = makeMockContext();
@@ -205,6 +206,7 @@ public class MultiSurfaceThematicisationTaskTest extends TestCase {
 
   }
 
+  @Test
   public void testImplementChangesAndPush() throws IllegalAccessException, URISyntaxException {
 
     ModelContext context = makeMockContext();
