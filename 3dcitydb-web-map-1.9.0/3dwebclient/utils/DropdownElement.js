0a45be7d7b3e1ce257c0c7470fd226414ec99fed
Ayda Grisiute <ayda.grisiute@sec.ethz.ch>
2022-06-03 17:16:22 +0800

    Added missing functions (showChooseUses() and showChooseProgrammes) to process and put query results into UI as selection options.

--- 3dcitydb-web-map-1.9.0/3dwebclient/utils/DropdownElement.js
+++ 3dcitydb-web-map-1.9.0/3dwebclient/utils/DropdownElement.js
@@ -1,9 +1,9 @@
 function buildOntoQuery(object){
-	query = "PREFIX zo: <http://www.theworldavatar.com/ontology/ontozoning/OntoZoning.owl#> SELECT DISTINCT ?s WHERE { ?s rdfs:subClassOf zo:" + object + " }"
+	query = "PREFIX zo: <http://www.theworldavatar.com/ontology/ontozoning/OntoZoning.owl#> SELECT DISTINCT ?s WHERE { ?s rdfs:subClassOf zo:" + object + " } LIMIT 50"
 	return query
 }
 
-function getDropdownElements(object, selection_element) {
+function getDropdownElements(object, element_type, dropdown_type) {
 	$.ajax({
 		url:"http://localhost:9999/blazegraph/namespace/kb/sparql",
 		type: 'POST',
@@ -17,13 +17,23 @@ function getDropdownElements(object, selection_element) {
 			var results = data["results"]["bindings"];
 			for (index in results){
 				var checkbox_line = results[index]["s"]["value"];
-				checkbox_lines.push(removePrefix(checkbox_line))				
+				appendElement(removePrefix(checkbox_line), element_type, dropdown_type)
 			}
 			console.log(checkbox_lines)			
 		}
     });
-
 }
+
+function appendElement(line,element_type, dropdown_type){
+	var some_element = "<div>" +
+			"<div class=" + element_type + ">" +
+			"<input type='checkbox'>" +
+			"</div>" +
+			"<div class=" + element_type + ">" + line + "</div>" +
+			"</div>";
+		$(dropdown_type).append(some_element)
+}
+
 function removePrefix(result){
 	return result.split("#")[1]	
 }
