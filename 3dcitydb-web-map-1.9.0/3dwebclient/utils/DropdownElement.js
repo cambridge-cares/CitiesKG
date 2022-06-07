0817d2ae81c19b6cd048247f414af98b380077fe
Ayda Grisiute <ayda.grisiute@sec.ethz.ch>
2022-06-07 23:34:05 +0800

    UI Development: implement dynamically updating AssignGFA dropdown. Fix minor UI bugs.

--- 3dcitydb-web-map-1.9.0/3dwebclient/utils/DropdownElement.js
+++ 3dcitydb-web-map-1.9.0/3dwebclient/utils/DropdownElement.js
@@ -1,5 +1,5 @@
 function buildOntoQuery(object){
-	query = "PREFIX zo: <http://www.theworldavatar.com/ontology/ontozoning/OntoZoning.owl#> SELECT DISTINCT ?s WHERE { ?s rdfs:subClassOf zo:" + object + " }"
+	query = "PREFIX zo: <http://www.theworldavatar.com/ontology/ontozoning/OntoZoning.owl#> SELECT DISTINCT ?s WHERE { ?s rdfs:subClassOf zo:" + object + "}"
 	return query
 }
 
@@ -20,17 +20,38 @@ function getDropdownElements(object, element_type, dropdown_type) {
 				appendElement(removePrefix(checkbox_line), element_type, dropdown_type)
 			}
 		}
-    });
+	});
 }
 
 function appendElement(line,element_type, dropdown_type){
 	var some_element =
 			"<div>" +
-			"<div id='check' class='checkbox'><input type='checkbox' value="+ line + "></div>" +
+			"<div id='" + line + "' class='checkbox'>" +
+			"<input type='checkbox' onchange='updateGfaRows()'/></div>" +
 			"<div class=" + element_type + ">" + line + "</div>" +
 			"</div>";
-		$(dropdown_type).append(some_element)
+	$(dropdown_type).append(some_element)
 }
+
+function updateGfaRows(){
+	var parent = document.getElementById("assignGFA");
+	while (parent.childElementCount > 1) {
+		parent.removeChild(parent.lastChild);
+	}
+	var checkboxes = document.getElementsByClassName('checkbox')
+	for (let i = 0; i < checkboxes.length; i++) {
+		var checkbox = checkboxes.item(i).firstChild;
+		if (checkbox.checked) {
+			var clicked_element =
+					"<div>" +
+					"<div class='text'>" + checkboxes.item(i).id + "</div>" +
+					"<div class='text_input'><input type='text' maxLength='5' size='3'></div>" +
+					"</div>";
+			$("#assignGFA").append(clicked_element)
+		}
+	}
+}
+
 
 function removePrefix(result){
 	var element = result.split("#")[1]
