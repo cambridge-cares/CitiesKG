f1589a0edc288a5ddb5306b3fbc635d61168cd99
huayyi-tai <87304039+huayyi-tai@users.noreply.github.com>
2022-06-16 17:34:02 +0800

    Add kingslynnEPSG3857 to access agent mapping switch case

--- agents/src/main/java/uk/ac/cam/cares/twa/cities/AccessAgentMapping.java
+++ agents/src/main/java/uk/ac/cam/cares/twa/cities/AccessAgentMapping.java
@@ -32,6 +32,7 @@ public class AccessAgentMapping {
             case "http://www.theworldavatar.com:83/citieskg/namespace/berlin/sparql/": return AccessAgentMappingEnum.BERLIN.TARGET_RESOURCE_ID;
             case "http://www.theworldavatar.com:83/citieskg/namespace/singaporeEPSG24500/sparql/": return AccessAgentMappingEnum.SINGAPORE_EPSG_24500.TARGET_RESOURCE_ID;
             case "http://www.theworldavatar.com:83/citieskg/namespace/singaporeEPSG4326/sparql/": return AccessAgentMappingEnum.SINGAPORE_EPSG_4326.TARGET_RESOURCE_ID;
+            case "http://www.theworldavatar.com:83/citieskg/namespace/kingslynnEPSG3857/sparql/": return AccessAgentMappingEnum.KINGSLYNN_EPSG_3857.TARGET_RESOURCE_ID;
             default: return null;
         }
     }
