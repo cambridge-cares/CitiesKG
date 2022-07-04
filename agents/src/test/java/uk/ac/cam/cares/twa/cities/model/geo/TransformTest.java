974fd98c4b933309366f0f2c8860823b7b1c9380
huayyi-tai <hyt38@cam.ac.uk>
2022-07-04 18:01:23 +0800

    Add unit tests for Transform; test constructor, methods, buildpolygon, getenvelope

---
+++ agents/src/test/java/uk/ac/cam/cares/twa/cities/model/geo/TransformTest.java
@@ -0,0 +1,62 @@
+package uk.ac.cam.cares.twa.cities.model.geo;
+
+import org.gdal.ogr.Geometry;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class TransformTest {
+
+    @Test
+    public void testNewTransform() {
+        Transform transform = new Transform();
+        assertNotNull(transform);
+    }
+
+    @Test
+    public void testNewTransformMethods() {
+        Transform transform = new Transform();
+        assertEquals(5, transform.getClass().getDeclaredMethods().length);
+    }
+
+    @Test
+    public void testBuildPolygon() {
+        // test case where dimension = 2
+        double[][] geometry1 = {{0.0, 0.0}, {1.0, 0.0}, {0.0, 1.0}, {1.0, 1.0}, {0.0, 0.0}};
+        Geometry geom1 = Transform.buildPolygon(geometry1);
+        double[] res1 = new double[4];
+        geom1.GetEnvelope(res1);
+
+        assertEquals("POLYGON", geom1.GetGeometryName());
+        assertEquals(0.0, res1[0]);
+        assertEquals(1.0, res1[1]);
+        assertEquals(0.0, res1[2]);
+        assertEquals(1.0, res1[3]);
+
+        // test case where dimension = 3
+        double[][] geometry2 = {{0.0, 0.0, 0.0}, {1.0, 0.0, 0.0}, {0.0, 1.0, 0.0}, {1.0, 1.0, 0.0}, {0.0, 0.0, 0.0}};
+        Geometry geom2 = Transform.buildPolygon(geometry2);
+        double[] res2 = new double[6];
+        geom2.GetEnvelope3D(res2);
+
+        assertEquals("POLYGON", geom2.GetGeometryName());
+        assertEquals(0.0, res2[0]);
+        assertEquals(1.0, res2[1]);
+        assertEquals(0.0, res2[2]);
+        assertEquals(1.0, res2[3]);
+        assertEquals(0.0, res2[4]);
+        assertEquals(0.0, res2[5]);
+    }
+
+    @Test
+    public void testGetEnvelop() {
+        double[][] geometry = {{0.0, 0.0}, {1.0, 0.0}, {0.0, 1.0}, {1.0, 1.0}, {0.0, 0.0}};
+        double[] res = Transform.getEnvelop(geometry);
+
+        assertEquals(0.0, res[0]);
+        assertEquals(1.0, res[1]);
+        assertEquals(0.0, res[2]);
+        assertEquals(1.0, res[3]);
+    }
+}
