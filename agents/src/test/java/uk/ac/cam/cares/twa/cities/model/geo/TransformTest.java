f6997c3e29564f8ffb40dcfd815394202876afd6
huayyi-tai <hyt38@cam.ac.uk>
2022-07-05 11:50:47 +0800

    add test for reprojectPoint and reprojectEnvelope

--- agents/src/test/java/uk/ac/cam/cares/twa/cities/model/geo/TransformTest.java
+++ agents/src/test/java/uk/ac/cam/cares/twa/cities/model/geo/TransformTest.java
@@ -3,6 +3,8 @@ package uk.ac.cam.cares.twa.cities.model.geo;
 import org.gdal.ogr.Geometry;
 import org.junit.jupiter.api.Test;
 
+import java.text.DecimalFormat;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
@@ -58,5 +60,25 @@ public class TransformTest {
         assertEquals(1.0, res[1]);
         assertEquals(0.0, res[2]);
         assertEquals(1.0, res[3]);
+    }
+
+    @Test
+    public void testReprojectPoint() {
+        double[] centroid = {52.5, 13.3};
+        double[] res = Transform.reprojectPoint(centroid, 4326, 25833);
+        DecimalFormat df = new DecimalFormat("#.#####");
+        assertEquals(384603.15117, Double.valueOf(df.format(res[0])));
+        assertEquals(5818010.35914, Double.valueOf(df.format(res[1])));
+    }
+
+    @Test
+    public void testReprojectEnvelope() {
+        double[] envelope = {52.5, 52.7, 13.3, 13.5};
+        double[] res = Transform.reprojectEnvelope(envelope, 4326, 25833);
+        DecimalFormat df = new DecimalFormat("#.#####");
+        assertEquals(5817709.52628, Double.valueOf(df.format(res[0])));
+        assertEquals(5840254.64080, Double.valueOf(df.format(res[1])));
+        assertEquals(384603.15117, Double.valueOf(df.format(res[2])));
+        assertEquals(398641.14503, Double.valueOf(df.format(res[3])));
     }
 }
