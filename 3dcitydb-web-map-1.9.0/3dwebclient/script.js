da7d035a7babd8cb2f055d0b51f79fdbb96abb68
huayyi-tai <hyt38@cam.ac.uk>
2022-07-13 15:28:41 +0800

    bring changes from 131 branch (131-export-bounding-box-and-tiles)

--- 3dcitydb-web-map-1.9.0/3dwebclient/script.js
+++ 3dcitydb-web-map-1.9.0/3dwebclient/script.js
@@ -210,7 +210,7 @@ function initClient() {
     var city = (new URL(window.location.href)).searchParams.get('city');
     loadCity(city);
 
-    // Zoom to desired camera position and load layers if encoded in the url...	
+    // Zoom to desired camera position and load layers if encoded in the url...
     zoomToDefaultCameraPosition().then(function (info) {
         var layers = urlController.getLayersFromUrl(window.location.href, CitydbUtil, CitydbKmlLayer, Cesium3DTilesDataLayer, Cesium);
         loadLayerGroup(layers);
@@ -223,7 +223,7 @@ function initClient() {
             }
             addWebMapServiceProvider();
         }
-        
+
         var cesiumWorldTerrainString = urlController.getUrlParaValue('cesiumWorldTerrain', window.location.href, CitydbUtil);
         var maptilerTerrainString = urlController.getUrlParaValue('maptiler', window.location.href, CitydbUtil);
         if(cesiumWorldTerrainString == "true") {
@@ -274,6 +274,8 @@ function loadCity(city) {
         loadBerlin();
     } else if (city == 'kingslynn') {
         loadKingsLynn();
+    } else if (city == 'singapore') {
+        loadSingapore();
     }
 }
 
@@ -336,6 +338,26 @@ function loadKingsLynn() {
 
     // find relevant files and load layers
     getAndLoadLayers('exported_kingslynn');
+}
+
+function loadSingapore() {
+    // set title
+    document.title = 'Singapore';
+
+    // set camera view
+    var cameraPostion = {
+        latitude: 1.286014,
+        longitude: 103.836364,
+        height: 2000,
+        heading: 345.2992773976952,
+        pitch: -44.26228062802528,
+        roll: 359.933888621294
+    }
+
+    flyToCameraPosition(cameraPostion);
+
+    // find relevant files and load layers
+    getAndLoadLayers('exported_singapore');
 }
 
 // send get request to server to discover files in specified folder, create and load layers
@@ -526,80 +548,83 @@ function listHighlightedObjects() {
 
 function computeDistance() {
     var iriArr = [];
-	var highlightedObjects = webMap.getAllHighlighted3DObjects();
-	var centroids = [];
-	for (var i = 0; i < highlightedObjects.length; i++) {
-		var entity = highlightedObjects[i][0];
-		console.log(entity.polygon);
-		var positions = entity.polygon.hierarchy._value.positions;
-		var center = Cesium.BoundingSphere.fromPoints(positions).center;
-		console.log(center);
-		centroids.push(center);
+    var highlightedObjects = webMap.getAllHighlighted3DObjects();
+    var centroids = [];
+    for (var i = 0; i < highlightedObjects.length; i++) {
+        var entity = highlightedObjects[i][0];
+        console.log(entity.polygon);
+        var positions = entity.polygon.hierarchy._value.positions;
+        var center = Cesium.BoundingSphere.fromPoints(positions).center;
+        console.log(center);
+        centroids.push(center);
         var iri = entity._iriPrefix + entity._name;
         iri = iri.endsWith('/') ? iri : iri + '/';
         iri = iri.replace(new RegExp('_(\\w*Surface)'), '');
-		iriArr.push(iri);
-	};
-	console.log(centroids);
+        iriArr.push(iri);
+    };
+    console.log(centroids);
 
     var redLine = cesiumViewer.entities.add({
-		name: "Distance line",
-		polyline: {
-			positions: centroids,
-			width: 2,
-			material: new Cesium.PolylineDashMaterialProperty({
-            color: Cesium.Color.WHITE,
-			dashLength: 15,
-          }),
-			cmalpToGround: true,
-		}
-	});
-	
+        name: "Distance line",
+        polyline: {
+            positions: centroids,
+            width: 2,
+            material: new Cesium.PolylineDashMaterialProperty({
+                color: Cesium.Color.WHITE,
+                dashLength: 15,
+            }),
+            clampToGround: true,
+        }
+    });
+
     var labelText = '';
     var label = cesiumViewer.entities.add({
         name: 'Distance label',
         position: getMidpoint(centroids[0], centroids[1]),
         label: {
-    font : 'bold 22px arial',
-    showBackground : false,
-    horizontalOrigin : Cesium.HorizontalOrigin.CENTER,
-    verticalOrigin : Cesium.VerticalOrigin.CENTER,
-    pixelOffset : new Cesium.Cartesian2(0, 20),
-    eyeOffset: new Cesium.Cartesian3(0,0,-50),
-	text: labelText,
-	fillColor : Cesium.Color.WHITE,
-	}
+            font : 'bold 22px arial',
+            showBackground : false,
+            heightReference :Cesium.HeightReference.CLAMP_TO_GROUND,
+            horizontalOrigin : Cesium.HorizontalOrigin.CENTER,
+            verticalOrigin : Cesium.VerticalOrigin.CENTER,
+            pixelOffset : new Cesium.Cartesian2(0, 20),
+            eyeOffset: new Cesium.Cartesian3(0,0,-50),
+            text: labelText,
+            fillColor : Cesium.Color.WHITE,
+            backgroundColor : new Cesium.Color(0.0, 0.0, 0.0, 0.7),
+            showBackground : true,
+        }
     });
 
     jQuery.ajax({
-		url:"http://localhost:8080/agents/distance",
-		type: 'POST',
-		data: JSON.stringify({iris: iriArr}),
-		dataType: 'json',
-		contentType: 'application/json',
-		success: function(data, status_message, xhr){
-			console.log(data["distances"]);
-			var distance = Math.round(data["distances"][0]);
-			label.label.text = distance.toString() + " " + "m";
-		}
-    });	
+        url:"http://localhost:8080/agents/distance",
+        type: 'POST',
+        data: JSON.stringify({iris: iriArr}),
+        dataType: 'json',
+        contentType: 'application/json',
+        success: function(data, status_message, xhr){
+            console.log(data["distances"]);
+            var distance = Math.round(data["distances"][0]);
+            label.label.text = distance.toString() + " " + "m";
+        }
+    });
 
 }
 
 function getMidpoint(point1, point2) {
-	var geodesic = new Cesium.EllipsoidGeodesic();
+    var geodesic = new Cesium.EllipsoidGeodesic();
     var scratch = new Cesium.Cartographic();
-	
-	var point1cart = Cesium.Cartographic.fromCartesian(point1);
-	var point2cart = Cesium.Cartographic.fromCartesian(point2);
-	
+
+    var point1cart = Cesium.Cartographic.fromCartesian(point1);
+    var point2cart = Cesium.Cartographic.fromCartesian(point2);
+
     geodesic.setEndPoints(point1cart, point2cart);
     var midpointCartographic = geodesic.interpolateUsingFraction(0.5, scratch);
     return Cesium.Cartesian3.fromRadians(midpointCartographic.longitude, midpointCartographic.latitude);
-	
+
 }
 
-	//---Extended Web-Map-Client part---//
+//---Extended Web-Map-Client part---//
 
 function listHiddenObjects() {
     var hidddenListElement = document.getElementById("citydb_hiddenlist");
@@ -1085,7 +1110,7 @@ function addNewLayer() {
         maxSizeOfCachedTiles: addLayerViewModel.maxSizeOfCachedTiles,
         maxCountOfVisibleTiles: addLayerViewModel.maxCountOfVisibleTiles
     }
-    
+
     // since Cesium 3D Tiles also require name.json in the URL, it must be checked first
     var layerDataTypeDropdown = document.getElementById("layerDataTypeDropdown");
     if (layerDataTypeDropdown.options[layerDataTypeDropdown.selectedIndex].value === 'Cesium 3D Tiles') {
@@ -1166,9 +1191,9 @@ function createScreenshot() {
     var imageUri = cesiumViewer.canvas.toDataURL();
     var imageWin = window.open("");
     imageWin.document.write("<html><head>" +
-            "<title>" + imageUri + "</title></head><body>" +
-            '<img src="' + imageUri + '"width="100%">' +
-            "</body></html>");
+        "<title>" + imageUri + "</title></head><body>" +
+        '<img src="' + imageUri + '"width="100%">' +
+        "</body></html>");
     return imageWin;
 }
 
@@ -1194,9 +1219,9 @@ function toggleTerrainShadows() {
         cesiumViewer.terrainShadows = Cesium.ShadowMode.ENABLED;
         if (!cesiumViewer.shadows) {
             CitydbUtil.showAlertWindow("OK", "Switching on terrain shadows now", 'Please note that shadows for 3D models will also be switched on.',
-                    function () {
-                        toggleShadows();
-                    });
+                function () {
+                    toggleShadows();
+                });
         }
     }
 }
