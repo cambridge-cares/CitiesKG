aea4607c6ebddbeca9ecc41d8ad6b299e026a4bf
sganguly-cares <srishti.ganguly@cares.cam.ac.uk>
2022-07-26 13:28:29 +0800

    dev-extend-KML-exporter-for-cityfurniture: Added new method in StatementTransformer.java

--- impexp-core/src/main/java/org/citydb/database/adapter/blazegraph/StatementTransformer.java
+++ impexp-core/src/main/java/org/citydb/database/adapter/blazegraph/StatementTransformer.java
@@ -70,17 +70,6 @@ public class StatementTransformer {
         return sparql;
     }
 
-    //query = WHERE\n { ?id ocgml:id ? .}";//StatementTransformer.getGenericCityObjectBasisData(currentLod);
-
-    /* CityFurniture */
-    public static String getCityFurnitureQuery(){
-        String sparql = "PREFIX  ocgml: <" + PREFIX_ONTOCITYGML + ">\n" +
-                "SELECT *\n" +
-                "FROM <" + IRI_GRAPH_BASE + "cityfurniture/" + ">\n" +
-                "WHERE\n { ?id ocgml:id ? .}";
-        return sparql;
-    }
-
     public static String getGenericCityObjectQuery(int lodToExportFrom, DisplayForm displayForm, boolean isImplicit, boolean exportAppearance){
         String query = null;
 
@@ -502,4 +491,12 @@ public class StatementTransformer {
         return union;
     }
 
+    /* CityFurniture */
+    public static String getCityFurnitureQuery(){
+        String sparql = "PREFIX  ocgml: <" + PREFIX_ONTOCITYGML + ">\n" +
+                "SELECT *\n" +
+                "FROM <" + IRI_GRAPH_BASE + "cityfurniture/" + ">\n" +
+                "WHERE\n { ?id ocgml:id ? .}";
+        return sparql;
+    }
 }
