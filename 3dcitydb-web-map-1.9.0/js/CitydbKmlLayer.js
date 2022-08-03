4db4dd22c357f4b70679a8e259d7e8652f9a743d
shiyingli <shiying.li@sec.ethz.ch>
2022-08-03 16:02:44 +0800

    Using local CIA and change the hovercolor to yellow

--- 3dcitydb-web-map-1.9.0/js/CitydbKmlLayer.js
+++ 3dcitydb-web-map-1.9.0/js/CitydbKmlLayer.js
@@ -73,7 +73,7 @@
         this._viewChangedEvent = new Cesium.Event();
 
         this._highlightColor = new Cesium.Color(65/255, 168/255, 255/255, 0.8);
-        this._mouseOverhighlightColor = new Cesium.Color(130/255, 187/255, 255/255, 1.0);
+        this._mouseOverhighlightColor = new Cesium.Color(255/255, 255/255, 126/255, 1.0); // original: (130/255, 187/255, 255/255, 1.0)
 
 
         this._layerDataType = options.layerDataType;
