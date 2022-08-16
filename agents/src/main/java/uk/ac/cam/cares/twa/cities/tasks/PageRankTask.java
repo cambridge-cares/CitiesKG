9a080d9b8ee58df7733461b188cc97b66a4a902f
Arkadiusz Chadzynski <arkadiusz.chadzynski@cares.cam.ac.uk>
2022-08-16 17:53:11 +0800

    Added algorithm implementation.

--- agents/src/main/java/uk/ac/cam/cares/twa/cities/tasks/PageRankTask.java
+++ agents/src/main/java/uk/ac/cam/cares/twa/cities/tasks/PageRankTask.java
@@ -1,8 +1,25 @@
 package uk.ac.cam.cares.twa.cities.tasks;
 
+import com.hp.hpl.jena.graph.Node;
+import com.hp.hpl.jena.rdf.model.Model;
+import com.hp.hpl.jena.rdf.model.ModelFactory;
+import com.hp.hpl.jena.rdf.model.RDFNode;
+import com.hp.hpl.jena.rdf.model.ResourceFactory;
+import com.hp.hpl.jena.rdf.model.Statement;
+import com.hp.hpl.jena.rdf.model.impl.ResourceImpl;
+import edu.uci.ics.jung.algorithms.scoring.PageRank;
+import edu.uci.ics.jung.graph.Graph;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.Writer;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.concurrent.BlockingQueue;
+import net.rootdev.jenajung.JenaJungGraph;
 import org.json.JSONArray;
+import org.json.JSONObject;
 import org.semanticweb.owlapi.model.IRI;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
 import uk.ac.cam.cares.twa.cities.agents.GraphInferenceAgent;
@@ -42,7 +59,48 @@ public class PageRankTask implements UninitialisedDataQueueTask {
     while (isRunning()) {
       while (!dataQueue.isEmpty()) {
         try {
-          //get data and run Page Rank
+
+
+          // get data
+          Map<String, JSONArray> map = this.dataQueue.take();
+          JSONArray data = map.get(this.taskIri.toString());
+
+          // convert to jung graph
+
+          Graph graph = createGraph(data);
+
+
+          //run Page Rank
+          PageRank<RDFNode, Integer> ranker = new PageRank<RDFNode, Integer>(graph, 0.3);
+          ranker.evaluate();
+
+          Map<Node, Double> result = new HashMap<>();
+          for (Object v : graph.getVertices()) {
+            result.put(((ResourceImpl) v).asNode(), ranker.getVertexScore((RDFNode) v));
+          }
+
+
+          //store results - @todo: save it to the KG
+
+          String eol = System.getProperty("line.separator");
+
+          try (Writer writer = new FileWriter(System.getProperty("java.io.tmpdir") + "/somefile.csv")) {
+            writer.append("Link")
+                .append(',')
+                .append("Score")
+                .append(eol);
+            for (Map.Entry<Node, Double> entry : result.entrySet()) {
+              writer.append("\"" + entry.getKey().toString() + "\"")
+                  .append(',')
+                  .append(new BigDecimal(entry.getValue().doubleValue()).setScale(20, RoundingMode.FLOOR).toString())
+                  .append(eol);
+            }
+            System.out.println(System.getProperty("java.io.tmpdir") + "/somefile.csv");
+          } catch (IOException ex) {
+            ex.printStackTrace(System.err);
+          }
+
+
         } catch (Exception e) {
           throw new JPSRuntimeException(e);
         } finally {
@@ -51,5 +109,19 @@ public class PageRankTask implements UninitialisedDataQueueTask {
       }
     }
   }
+
+  public Graph<RDFNode, Statement> createGraph(JSONArray array) {
+    Model model = ModelFactory.createDefaultModel();
+
+    for (Object data : array) {
+      JSONObject obj = (JSONObject) data;
+      model.add(ResourceFactory.createResource(obj.getString("s")),
+          ResourceFactory.createProperty(obj.getString("p")),
+              ResourceFactory.createResource(obj.getString("o")));
+    }
+
+    return new JenaJungGraph(model);
+  }
+
 
 }
