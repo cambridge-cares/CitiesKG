78585b40ad3cdf2853f99b9ced12fe5ca8d54eb4
Arkadiusz Chadzynski <arkadiusz.chadzynski@cares.cam.ac.uk>
2022-08-18 17:53:09 +0800

    Recactored code to store data in the KG.

--- agents/src/main/java/uk/ac/cam/cares/twa/cities/tasks/PageRankTask.java
+++ agents/src/main/java/uk/ac/cam/cares/twa/cities/tasks/PageRankTask.java
@@ -1,33 +1,27 @@
 package uk.ac.cam.cares.twa.cities.tasks;
 
-import com.hp.hpl.jena.datatypes.RDFDatatype;
-import com.hp.hpl.jena.datatypes.xsd.XSDDatatype;
-import com.hp.hpl.jena.graph.Node;
-import com.hp.hpl.jena.graph.Triple;
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.RDFNode;
 import com.hp.hpl.jena.rdf.model.ResourceFactory;
-import com.hp.hpl.jena.rdf.model.Statement;
-import com.hp.hpl.jena.rdf.model.impl.ResourceImpl;
 import edu.uci.ics.jung.algorithms.scoring.PageRank;
 import edu.uci.ics.jung.graph.Graph;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.Writer;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Map.Entry;
+import java.util.ResourceBundle;
 import java.util.UUID;
 import java.util.concurrent.BlockingQueue;
 import net.rootdev.jenajung.JenaJungGraph;
 import org.apache.jena.arq.querybuilder.UpdateBuilder;
+import org.apache.jena.datatypes.xsd.XSDDatatype;
+import org.apache.jena.graph.Node;
+import org.apache.jena.graph.NodeFactory;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import org.semanticweb.owlapi.model.IRI;
 import uk.ac.cam.cares.jps.base.exception.JPSRuntimeException;
+import uk.ac.cam.cares.jps.base.query.AccessAgentCaller;
 import uk.ac.cam.cares.twa.cities.agents.GraphInferenceAgent;
 
 /**
@@ -65,49 +59,19 @@ public class PageRankTask implements UninitialisedDataQueueTask {
     while (isRunning()) {
       while (!dataQueue.isEmpty()) {
         try {
-
-
           // get data
           Map<String, JSONArray> map = this.dataQueue.take();
           JSONArray data = map.get(this.taskIri.toString());
 
           // convert to jung graph
-
           Graph graph = createGraph(data);
 
-
           //run Page Rank
-          PageRank<RDFNode, Integer> ranker = new PageRank<RDFNode, Integer>(graph, 0.3);
+          PageRank<RDFNode, Double> ranker = new PageRank<RDFNode, Double>(graph, 0.3);
           ranker.evaluate();
 
-          Map<Node, Double> result = new HashMap<>();
-          for (Object v : graph.getVertices()) {
-            result.put(((ResourceImpl) v).asNode(), ranker.getVertexScore((RDFNode) v));
-          }
-
-
-          //store results - @todo: save it to the KG
-
-          UpdateBuilder ub = new UpdateBuilder();
-
-          ub.addPrefix(GraphInferenceAgent.ONINF_PREFIX, GraphInferenceAgent.ONINF_SCHEMA);
-
-          String inGraph = "http://127.0.0.1:9999/blazegraph/namespace/singaporeEPSG4326/sparql/OntoInfer/";
-          for (Entry<Node, Double> entry : result.entrySet()) {
-            String id = inGraph + UUID.randomUUID();
-            ub.addInsert(inGraph, id, GraphInferenceAgent.ONINF_PREFIX + ":hasInferenceObject",
-                entry.getKey().toString());
-            ub.addInsert(inGraph, id, GraphInferenceAgent.ONINF_PREFIX + ":hasInferenceAlgorithm",
-                GraphInferenceAgent.ONINF_SCHEMA + "PageRankAlgorithm");
-            ub.addInsert(inGraph, id, GraphInferenceAgent.ONINF_PREFIX + ":oninf:hasInferredValue",
-                String.valueOf(new BigDecimal(entry.getValue().doubleValue()).setScale(20, RoundingMode.FLOOR)));
-            System.out.println(ub.build().toString());
-
-            //@todo: insert to KG
-          }
-
-
-
+          //store results
+          storeResults(graph, ranker);
 
         } catch (Exception e) {
           throw new JPSRuntimeException(e);
@@ -118,7 +82,7 @@ public class PageRankTask implements UninitialisedDataQueueTask {
     }
   }
 
-  public Graph<RDFNode, Statement> createGraph(JSONArray array) {
+  private JenaJungGraph createGraph(JSONArray array) {
     Model model = ModelFactory.createDefaultModel();
 
     for (Object data : array) {
@@ -131,5 +95,39 @@ public class PageRankTask implements UninitialisedDataQueueTask {
     return new JenaJungGraph(model);
   }
 
+  //@todo: remove hardcoding
+  private void storeResults(Graph graph, PageRank<RDFNode, Double>  ranker) {
+
+    Node inGraph = NodeFactory.createURI( "http://127.0.0.1:9999/blazegraph/namespace/singaporeEPSG4326/sparql/OntoInfer/");
+
+    UpdateBuilder ub = new UpdateBuilder();
+    ub.addPrefix(GraphInferenceAgent.ONINF_PREFIX, GraphInferenceAgent.ONINF_SCHEMA);
+    int counter = 0;
+
+    for (Object v : graph.getVertices()) {
+      //prepare inference update for a node
+      Node id = NodeFactory.createURI(inGraph.getURI() + UUID.randomUUID());
+      ub.addInsert(inGraph, id, GraphInferenceAgent.ONINF_PREFIX + ":hasInferenceObject",
+          NodeFactory.createURI(String.valueOf(v)));
+      ub.addInsert(inGraph, id, GraphInferenceAgent.ONINF_PREFIX + ":hasInferenceAlgorithm",
+          NodeFactory.createURI(GraphInferenceAgent.ONINF_SCHEMA + "PageRankAlgorithm"));
+      ub.addInsert(inGraph, id, GraphInferenceAgent.ONINF_PREFIX + ":hasInferredValue",
+          NodeFactory.createLiteral(String.valueOf(
+                  new BigDecimal(ranker.getVertexScore((RDFNode) v)).setScale(20, RoundingMode.FLOOR)),
+              XSDDatatype.XSDdouble));
+      counter++;
+
+      //store data if counter of nodes is 100 and start over
+      if (counter >= 100) {
+        System.out.println(ub.build().toString());
+        AccessAgentCaller.updateStore(ResourceBundle.getBundle("config").getString("uri.route"),
+            ub.build().toString());
+        ub = new UpdateBuilder();
+        ub.addPrefix(GraphInferenceAgent.ONINF_PREFIX, GraphInferenceAgent.ONINF_SCHEMA);
+        counter = 0;
+      }
+    }
+
+  }
 
 }
