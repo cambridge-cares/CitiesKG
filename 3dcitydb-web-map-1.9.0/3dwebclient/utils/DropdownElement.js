c27d0aad3fbbab4fb17033b4a3668720f36eced8
shiyingli <shiying.li@sec.ethz.ch>
2022-09-23 18:07:35 +0800

    Added an icon for the disclaimer

--- 3dcitydb-web-map-1.9.0/3dwebclient/utils/DropdownElement.js
+++ 3dcitydb-web-map-1.9.0/3dwebclient/utils/DropdownElement.js
@@ -348,3 +348,46 @@ function resetAllInputs(){
 	totalGfaInput.value = '';
 }
 
+function addDisclaimerButton(){
+	var toolbar = document.getElementsByClassName('cesium-viewer-toolbar');  // HTMLCollection; has to use item(0) to add an additional icon
+
+	var qMarkSymbol = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+	qMarkSymbol.setAttribute("class", "cesium-svgPath-svg");
+	qMarkSymbol.setAttribute("width", "32");
+	qMarkSymbol.setAttribute("height", "32");
+	qMarkSymbol.setAttribute("viewBox", "0 0 32 32");
+	qMarkSymbol.setAttribute("style", "none");
+	qMarkSymbol.innerHTML = "<path d=" + "M16,1.466C7.973,1.466,1.466,7.973,1.466,16c0,8.027,6.507,14.534,14.534,14.534c8.027,0,14.534-6.507,14.534-14.534C30.534,7.973,24.027,1.466,16,1.466z" + "M17.328,24.371h-2.707v-2.596h2.707V24.371zM17.328,19.003v0.858h-2.707v-1.057c0-3.19,3.63-3.696,3.63-5.963c0-1.034-0.924-1.826-2.134-1.826c-1.254,0-2.354,0.924-2.354,0.924l-1.541-1.915c0,0,1.519-1.584,4.137-1.584c2.487,0,4.796,1.54,4.796,4.136C21.156,16.208,17.328,16.627,17.328,19.003z>";
+
+	var disclaimerButton = document.createElement('button');
+	disclaimerButton.type = 'button';
+	disclaimerButton.className = 'cesium-button cesium-toolbar-button cesium-navigation-help-button';
+	disclaimerButton.setAttribute('data-bind', 'attr: { title: tooltip }');
+	disclaimerButton.appendChild(qMarkSymbol);
+	
+	
+	toolbar.item(0).appendChild(disclaimerButton);
+
+	var instructionContainer = document.createElement('div');
+	instructionContainer.id = "instructionContainer";
+	instructionContainer.className = 'cesium-navigation-help';
+	instructionContainer.setAttribute('data-bind', 'css: { "cesium-navigation-help-visible" : showInstructions}');
+	toolbar.item(0).appendChild(instructionContainer);
+
+	var mouseButton = document.createElement('button');
+	mouseButton.type = 'button';
+	mouseButton.className = 'cesium-navigation-button cesium-navigation-button-left';
+	mouseButton.setAttribute('data-bind', 'click: showClick, css: {"cesium-navigation-button-selected": !_touch, "cesium-navigation-button-unselected": _touch}');
+	mouseButton.appendChild(document.createTextNode('Mouse'));
+
+	var touchButton = document.createElement('button');
+	touchButton.type = 'button';
+	touchButton.className = 'cesium-navigation-button cesium-navigation-button-right';
+	touchButton.setAttribute('data-bind', 'click: showTouch, css: {"cesium-navigation-button-selected": _touch, "cesium-navigation-button-unselected": !_touch}');
+	touchButton.appendChild(document.createTextNode('Touch'));
+
+	instructionContainer.appendChild(mouseButton);
+	instructionContainer.appendChild(touchButton);
+
+}
+
