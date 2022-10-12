5db9be226321b1239b466dd5c59c88b052268112
shiyingli <shiying.li@sec.ethz.ch>
2022-10-12 16:35:01 +0800

    Added image icon for the Infobox

--- 3dcitydb-web-map-1.9.0/3dwebclient/utils/DropdownElement.js
+++ 3dcitydb-web-map-1.9.0/3dwebclient/utils/DropdownElement.js
@@ -363,16 +363,17 @@ function addDisclaimerButton(){
 	qMarkSymbol.setAttribute("height", "32");
 	qMarkSymbol.setAttribute("viewBox", "0 0 32 32");
 	qMarkSymbol.setAttribute("style", "none");
-	qMarkSymbol.innerHTML = "<path d=" + "M16,1.466C7.973,1.466,1.466,7.973,1.466,16c0,8.027,6.507,14.534,14.534,14.534c8.027,0,14.534-6.507,14.534-14.534C30.534,7.973,24.027,1.466,16,1.466z" + "M17.328,24.371h-2.707v-2.596h2.707V24.371zM17.328,19.003v0.858h-2.707v-1.057c0-3.19,3.63-3.696,3.63-5.963c0-1.034-0.924-1.826-2.134-1.826c-1.254,0-2.354,0.924-2.354,0.924l-1.541-1.915c0,0,1.519-1.584,4.137-1.584c2.487,0,4.796,1.54,4.796,4.136C21.156,16.208,17.328,16.627,17.328,19.003z>";
+	//qMarkSymbol.innerHTML = "<path d=M16,1.466C7.973,1.466,1.466,7.973,1.466,16c0,8.027,6.507,14.534,14.534,14.534c8.027,0,14.534-6.507,14.534-14.534C30.534,7.973,24.027,1.466,16,1.466z" + "M17.328,24.371h-2.707v-2.596h2.707V24.371zM17.328,19.003v0.858h-2.707v-1.057c0-3.19,3.63-3.696,3.63-5.963c0-1.034-0.924-1.826-2.134-1.826c-1.254,0-2.354,0.924-2.354,0.924l-1.541-1.915c0,0,1.519-1.584,4.137-1.584c2.487,0,4.796,1.54,4.796,4.136C21.156,16.208,17.328,16.627,17.328,19.003z>";
 
-	//var instructionIcon = document.createElement('img');
-	//instructionIcon.src = '../utils/image-source/instructionIcon.png';
+	var instructionIcon = document.createElement('img');
+	instructionIcon.src = '../3dwebclient/utils/image-source/infoIcon.png';
+	instructionIcon.style = "width:32px; height:32px;";
 
 	var disclaimerButton = document.createElement('button');
 	disclaimerButton.type = 'button';
 	disclaimerButton.className = 'cesium-button cesium-toolbar-button cesium-navigation-help-button';
 	disclaimerButton.setAttribute('data-bind', 'attr: { title: tooltip }');
-	disclaimerButton.appendChild(qMarkSymbol);
+	disclaimerButton.appendChild(instructionIcon);
 
 	wrapper.appendChild(disclaimerButton);
 
