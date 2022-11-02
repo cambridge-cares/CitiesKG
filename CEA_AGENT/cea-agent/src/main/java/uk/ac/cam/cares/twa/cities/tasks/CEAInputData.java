8be808d75185cf0577822459272b64d93718916e
Yi-Kai Tsai <yikai.tsai@cares.cam.ac.uk>
2022-11-02 16:44:53 +0800

    1375-dev-add-building-usage-input-to-cea-agent: add usage to CEAInputData

--- CEA_AGENT/cea-agent/src/main/java/uk/ac/cam/cares/twa/cities/tasks/CEAInputData.java
+++ CEA_AGENT/cea-agent/src/main/java/uk/ac/cam/cares/twa/cities/tasks/CEAInputData.java
@@ -3,9 +3,11 @@ package uk.ac.cam.cares.twa.cities.tasks;
 public class CEAInputData {
     public String geometry;
     public String height;
+    public String usage;
 
-    public CEAInputData(String geometry_value, String height_value) {
+    public CEAInputData(String geometry_value, String height_value, String usage_value) {
         this.geometry = geometry_value;
         this.height = height_value;
+        this.usage = usage_value;
     }
 }
