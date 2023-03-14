82a28838fc26345dad3c1db65de256733631d237
shiyingli <shiying.li@sec.ethz.ch>
2023-03-14 11:31:06 +0800

    Modularized the code

--- 3dcitydb-web-map-1.9.0/3dwebclient/utils/ProcessAgentResult.js
+++ 3dcitydb-web-map-1.9.0/3dwebclient/utils/ProcessAgentResult.js
@@ -1,5 +1,194 @@
 /*********************** Extension of script.js *******************************/
 
+
+/************************ Process Query Results ******************************/
+//Shiying: highlight multiple cityobjects, create customDatasource, pinCityobjects
+function processFilteredObjects(cityObjectsArray, colorStr){  // citydbKmlLayer object, list of files in the folder--> get the summaryfile
+    //var cityObjectsArray = ["UUID_fddf5c91-cdd6-436a-95e6-aa1fa199b75d", "UUID_e5779fd5-ea90-4d2c-9a0a-cf7f46e5aad3"];
+    //var cityObjectsArray = ["http://www.theworldavatar.com:83/citieskg/namespace/singaporeEPSG4326/sparql/cityobject/UUID_fddf5c91-cdd6-436a-95e6-aa1fa199b75d/", "http://www.theworldavatar.com:83/citieskg/namespace/singaporeEPSG4326/sparql/cityobject/UUID_e5779fd5-ea90-4d2c-9a0a-cf7f46e5aad3/", "http://www.theworldavatar.com:83/citieskg/namespace/singaporeEPSG4326/sparql/cityobject/UUID_b6f4d0de-cf5c-4917-aba0-c1a91fa4960b/"];
+
+    // UUID_fddf5c91-cdd6-436a-95e6-aa1fa199b75d - inside
+    // UUID_b6f4d0de-cf5c-4917-aba0-c1a91fa4960b - outside of the scene
+
+    var currentLayer = webMap.activeLayer;
+    var filteredResult= {};
+    var highlightColor = currentLayer._highlightColor; // new Cesium.Color(16/255, 77/255, 151/255, 1.0);
+
+    for (let i = 0; i < cityObjectsArray.length; i++) {
+        var strArray = cityObjectsArray[i].split("/");
+        var gmlid = strArray[strArray.length-2];
+        filteredResult[gmlid] = highlightColor; // new Cesium.Color(65/255, 168/255, 255/255, 0.8);
+    }
+
+    filteredObjects = filteredResult;
+    pinHighlightObjects(cityObjectsArray, colorStr);
+    //highlightFilteredObj(filteredObjects);
+    return filteredObjects;
+}
+
+function highlightFilteredObj(filteredObjects){
+    var currentLayer = webMap.activeLayer;
+    currentLayer.unHighlightAllObjects();
+    if (filteredObjects != undefined){
+        currentLayer.highlight(filteredObjects);
+    }
+}
+
+function pinHighlightObjects(cityObjectsArray, hexColorString){
+    var gmlidArray = [];
+
+    for (let i = 0; i < cityObjectsArray.length; i++) {
+        var strArray = cityObjectsArray[i].split("/");
+        var gmlid = strArray[strArray.length-2];
+        gmlidArray.push(gmlid);
+    }
+
+    // get geolocation
+    var currentLayer = webMap._activeLayer;
+    var testcityobjectsJsonData;
+    if (Cesium.defined(currentLayer)) {
+        testcityobjectsJsonData = currentLayer.cityobjectsJsonData;
+    }
+
+    var dataUnifier = dataSourcePrefix + _customDataSourceCounter;
+    var customDataSource = new Cesium.CustomDataSource(dataUnifier);
+    var currentLayer = webMap.activeLayer;
+
+    for (let i = 0; i < gmlidArray.length; i++){
+        var obj = testcityobjectsJsonData[gmlidArray[i]];
+        var id = gmlidArray[i];
+        var lon = (obj.envelope[0] + obj.envelope[2]) / 2.0;
+        var lat = (obj.envelope[1] + obj.envelope[3]) / 2.0;
+        //console.log(gmlidArray[i] + ": " + lon + ", " + lat);
+
+        addPoint(customDataSource, id, lat, lon, currentLayer ,hexColorString);
+    }
+    //addEventListeners(customDataSource);
+    customDataSourceMap.set(dataUnifier, customDataSource);
+    cesiumViewer.dataSources.add(customDataSource);
+
+    //var testdataSource = customDataSource;
+    //var testentity = testdataSource.getEntitiesById('UUID_3422c065-c104-4a48-9536-0f30ff96c670');
+    //console.log(testentity);
+
+    /**
+     // In order to only show the current dataSource, we need to remove the previous first.
+     for (let [key, value] of customDataSourceMap){
+        console.log(key + " : " + cesiumViewer.dataSources.remove(value));
+    }**/
+}
+
+function addPoint(customDataSource, pointId, lat, long, parentLayer ,hexColorString){
+
+    customDataSource.entities.add({
+        position: Cesium.Cartesian3.fromDegrees(long, lat, 5),
+        id: pointId,
+        point: {
+            pixelSize: 10,
+            color: Cesium.Color.fromCssColorString(hexColorString),
+            outlineColor: Cesium.Color.BLACK,
+            outlineWidth:1,
+        },
+        layerId: parentLayer.id,
+        name: pointId,
+        iriPrefix: parentLayer._citydbKmlDataSource._iriPrefix,
+    });
+}
+
+
+/************************ Query Result Windows ******************************/
+// create the summary text for PPF result box
+// resultjson is json object
+
+function processInfoContext(resultjson){
+    let ifGFA = false;
+    var infoText = document.createElement("div");
+    var title = document.createElement("span");
+
+    var developmentType = document.getElementById("DevelopmentType");
+    title.innerHTML = "Search " + developmentType.options[developmentType.selectedIndex].text;
+    if (title.innerHTML === "Search Search plots...") {
+        title.innerHTML = "Search example query"
+    }
+
+    title.style.fontWeight = "bold";
+    title.style.fontSize = "12px";
+    infoText.appendChild(title);
+    infoText.appendChild(document.createElement("br"));
+
+    //{"allowsProgramme":{"Cafe":"","StudentRunBusiness":""},"TotalGFA":""}
+    var filteredCount = resultjson["http://www.theworldavatar.com:83/access-agent/access"]["filteredCounts"];
+    var inputs = resultjson["context"]["http://www.theworldavatar.com:83/access-agent/access"];
+    var allowOption;
+    var allowsObjects = [];
+    var allowsGFA = [];
+    if (Object.keys(inputs).includes("allowsProgramme")){
+        allowsObjects = Object.keys(inputs["allowsProgramme"]);
+        allowsGFA = Object.values(inputs["allowsProgramme"]);
+        allowOption = "allowsProgramme:";
+    } else if (Object.keys(inputs).includes("allowsUse")){
+        allowsObjects = Object.keys(inputs["allowsUse"]);
+        allowsGFA = Object.values(inputs["allowsUse"]);
+        allowOption = "allowsUse:";
+    }
+    var allowOpts = document.createElement("span");
+    allowOpts.textContent = allowOption;
+    infoText.appendChild(allowOpts);
+    infoText.appendChild(document.createElement("br"));
+
+    var allowOptText = document.createElement("span");
+    var allowsContent = "";
+    for (i = 0; i < allowsObjects.length; ++i){
+        if (allowsGFA[i] != ""){
+            ifGFA = true;
+            allowsContent += allowsObjects[i] + ": " + allowsGFA[i] + " sqm" + "<br />";
+        }else{
+            allowsContent += allowsObjects[i] + "<br/>";
+        }
+
+    }
+    allowOptText.innerHTML = allowsContent;
+    infoText.appendChild(allowOptText);
+    //infoText.appendChild(document.createElement("br"));
+
+    if (Object.keys(inputs).includes("TotalGFA") && inputs["TotalGFA"] > 0){
+        ifGFA = true;
+        var totalGFA = document.createElement("span");
+        var totalGFAValue = inputs["TotalGFA"];
+        totalGFA.textContent = "Total GFA: " + totalGFAValue + " sqm";
+        infoText.appendChild(totalGFA);
+        infoText.appendChild(document.createElement("br"));
+    }
+
+    var objCount = document.createElement("span");
+    objCount.textContent = "# search results: " + filteredCount + " plots";
+    infoText.appendChild(document.createElement("br"));
+    infoText.appendChild(objCount);
+    infoText.appendChild(document.createElement("br")); // Ayda: swapped white line positions according Pieter's instructions.
+
+    // static part: measure the impact of the demo
+    var docCount = document.createElement("span");
+    if (ifGFA){
+        docCount.innerHTML = "The search replaces manually checking <br /> 38 regulatory documents";
+    }else {
+        docCount.innerHTML = "The search replaces manually checking <br /> 17 regulatory documents ";
+    }
+    infoText.appendChild(document.createElement("br"));
+    infoText.appendChild(docCount);
+    var testURL = document.createElement("a");
+    testURL.href = "http://www.theworldavatar.com:83/citieskg/#explore";
+    testURL.target = "_blank";
+    testURL.text = "http://www.theworldavatar.com:83/citieskg/#explore";
+    testURL.onclick
+    infoText.appendChild(testURL);
+    return infoText;
+}
+
+
+
+
+
+
 /************************ Process CIA Agent Results **************************************/
 
 function showResultWindow(resultJson, colorStr){
