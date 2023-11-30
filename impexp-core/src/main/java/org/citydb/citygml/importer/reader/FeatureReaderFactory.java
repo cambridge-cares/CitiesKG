3686b34263c16379b53dfcad4c5f0cf573a6f769
shiyingli <shiying.li@sec.ethz.ch>
2023-11-30 21:45:12 +0800

    restructured the directories

--- impexp-core/src/main/java/org/citydb/citygml/importer/reader/FeatureReaderFactory.java
+++
@@ -1,9 +0,0 @@
-package org.citydb.citygml.importer.reader;
-
-import org.citydb.citygml.importer.filter.CityGMLFilter;
-import org.citydb.config.Config;
-
-public interface FeatureReaderFactory {
-    void initializeContext(CityGMLFilter filter, Config config) throws FeatureReadException;
-    FeatureReader createFeatureReader() throws FeatureReadException;
-}
