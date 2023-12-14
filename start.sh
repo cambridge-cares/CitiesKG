436a9d9e365ba7cd378466371fa042314a88a9fd
CKG <CKG@SEC-L10657.sec.local>
2023-12-14 11:57:25 +0800

    Added automation script to start the initialization

--- start.sh
+++ start.sh
@@ -1,12 +1,18 @@
 #! /bin/bash
 
+docker compose up -d --no-build
 # Step 1: Initalize Access Agent by execute the upload the routing file in access_agent_setup directory
 # Make sure that the routing information is correct in routing.json
 # more information can be found in : https://github.com/cambridge-cares/TheWorldAvatar/tree/main/Agents/AccessAgent
+#echo $?
 
-# bash access_agent_setup/uploadRouting.sh
+#wait
+sleep 5
+cd ./access_agent_setup/
+bash uploadRouting.sh
 
 # Step 2: Download the singaporeEPSG4326 dataset from URL: https://drive.google.com/file/d/169-tc6nKTetAynpA36stfD6epmBFFF2_/view?usp=sharing
+cd ..
 #fileid="169-tc6nKTetAynpA36stfD6epmBFFF2_"
 #filename="singaporeEPSG4326.zip"
 #html=`curl -c ./cookie -s -L "https://drive.google.com/uc?export=download&id=${fileid}"`
