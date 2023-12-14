52a254a6404ddf2340bec3eb106aa29d4b6d9d7d
shiyingli <shiying.li@sec.ethz.ch>
2023-12-14 18:52:43 +0800

    Changed the folder structure of CitiesKG

--- ckg_agents_code/impexp-core/src/main/java/org/citydb/database/connection/ConnectionManager.java
+++
@@ -1,8 +0,0 @@
-package org.citydb.database.connection;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-
-public interface ConnectionManager {
-    Connection getConnection() throws SQLException;
-}
--- ckg_agents_code/impexp-plugin-api/src/main/java/org/citydb/plugin/extension/Extension.java
+++
@@ -1,4 +0,0 @@
-package org.citydb.plugin.extension;
-
-public interface Extension {
-}
