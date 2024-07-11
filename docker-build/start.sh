1951b39c1cb69aea4c0eabf921a97954fcb8c51a
shiyingli <shiying.li@sec.ethz.ch>
2024-07-11 22:35:15 +0800

    Added the step 3, execute batch file from shell script

--- docker-build/start.sh
+++ docker-build/start.sh
@@ -1,15 +1,18 @@
 #! /bin/bash
 
 # Step 0: Spin up the container, which execute the docker-compose.yml
+echo "Step 0: Spin up the container, which execute the docker-compose.yml"
 docker compose up -d --no-build
+
 # Step 1: Initalize Access Agent by execute the upload the routing file in access_agent_setup directory
 # Make sure that the routing information is correct in routing.json
 # more information can be found in : https://github.com/cambridge-cares/TheWorldAvatar/tree/main/Agents/AccessAgent
 #echo $?
 
-#wait
+
+#echo "Step 1: Initalize Access Agent by execute the upload the routing file in access_agent_setup directory"
 sleep 5
-cd ./access_agent_setup/
+cd ../access_agent_setup/
 bash uploadRouting.sh
 
 # Step 2: Download the singaporeEPSG4326 dataset from URL: https://drive.google.com/file/d/169-tc6nKTetAynpA36stfD6epmBFFF2_/view?usp=sharing
@@ -23,7 +26,8 @@ bash uploadRouting.sh
 #tar -xvf singaporeEPSG4326.tar.gz -C ./3dwebclient
 
 # Step 3: start the batch for blazegraph
-#cd D:\blazegraphDB
-#`cmd`
-#Start_Blazegraph_with_default_settings.bat
+echo "Step 3: start the batch for blazegraph"
+cd D:/blazegraphDB/blazegraph
+#start "cmd.exe" /c Start_Blazegraph_with_default_settings.bat
+./Start_Blazegraph_with_default_settings.bat
 
